a25334b0d1f3aa62d8db5fa5e7e5ed8d5c8f44c6
winiar93 <winiar93@gmail.com>
2024-06-22 14:20:34 +0200

    Added strategy_rules.py

---
+++ strategy_rules.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class GameOfLifeRule(ABC):
+    @abstractmethod
+    def apply(self, cells, row, col):
+        pass
+
+
+class DayAndNightRule(GameOfLifeRule):
+    def apply(self, cells, row, col):
+        alive_cells = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        if cells[row, col] == 1:
+            if alive_cells in {3, 4, 6, 7, 8}:
+                return 1
+            else:
+                return 0
+        else:
+            if alive_cells in {3, 6, 7, 8}:
+                return 1
+        return 0
+
+class StandardRule(GameOfLifeRule):
+    def apply(self, cells, row, col):
+        alive_cells = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        if cells[row, col] == 1:
+            if alive_cells < 2 or alive_cells > 3:
+                return 0
+            elif 2 <= alive_cells <= 3:
+                return 1
+        else:
+            if alive_cells == 3:
+                return 1
+        return 0
